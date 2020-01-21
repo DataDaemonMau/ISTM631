de802d4536eb3671c26cc793fa1a34d6217c1744
Mauricio Guzman <59806578+DataDaemonMau@users.noreply.github.com>
2020-01-20 22:11:06 -0800

    Add files via upload

---
+++ absoluteValueFunc.py
@@ -0,0 +1,14 @@
+'''
+    Author: Mauricio Guzman
+    Purpose: Simple program in lieu of using the abs function to
+    manually perform the mathematical operation and return
+    the absolute value of x.
+'''
+
+# Assign the value to x
+x = -4
+# perform mathematical formula to get the absolute value of x
+absoluteValue = (x ** 2) ** 0.5
+
+# output the absolute value of x
+print("The absolute value of x is: ", absoluteValue)
