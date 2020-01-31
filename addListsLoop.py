e7b06cfd78b9f7ab65307be85c6b833f71bcbc74
Mauricio Guzman <59806578+DataDaemonMau@users.noreply.github.com>
2020-01-30 17:06:36 -0800

    Create addListsLoop.py

---
+++ addListsLoop.py
@@ -0,0 +1,14 @@
+'''
+  Author: Mauricio Guzman
+  Purpose: The purpose of this program is to loop through a defined list and return the sum of 
+  its numbers
+'''
+// define list to be looped through
+data = [1,2,3,4,5,6,7,8,9]
+// create for loop to loop through the individual elements
+for index in range(len(data)):
+  sumOfData += data[index]
+  print(sumOfData)
+  
+print("The sum of the referenced list is: ", sumOfData)
+  
